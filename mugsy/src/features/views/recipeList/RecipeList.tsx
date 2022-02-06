674d585f2fe59ee5756a8ffef1f4986013781d31
Matt Argyle <matt.coding@gmail.com>
2022-02-05 21:07:22 -0500

    update recipeList slice and start rendering/layout of listItems

--- mugsy/src/features/views/recipeList/RecipeList.tsx
+++ mugsy/src/features/views/recipeList/RecipeList.tsx
@@ -1,11 +1,42 @@
 import { BrowserRouter } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useAppSelector, useAppDispatch } from '../../../app/hooks';
+import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import styles from './landerCard.module.scss';
+import { getRecipeList } from './recipeListSlice'
 
 export const RecipeList: React.FC = () => {
+  const recipeList = useAppSelector(getRecipeList);
+  const dispatch = useAppDispatch();
+  let populatedRecipeList: any = [];
+
+  const populateList = () => {
+    if (recipeList !== undefined) {
+      recipeList.forEach((listItem) => {
+
+        console.log(listItem)
+      }
+      );
+      return populatedRecipeList;
+    }
+    else {
+      console.log('undefined');
+    }
+  }
+
+  useEffect(() => {
+    populateList();
+    console.log('firing');
+  });
+
+
   return (
     <div>
-    </div >
+      <Container>
+        {populatedRecipeList}
+      </Container>
+    </div>
   )
 };
