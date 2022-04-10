ee1c944d4874b95a5e5bc30ea6983a89ebd22566
Matt Argyle <matt.coding@gmail.com>
2022-04-10 16:05:58 -0400

    create base recipeBilder container, add scss file, add section buttons

--- mugsy/src/features/views/recipeBuilder/RecipeBuilder.tsx
+++ mugsy/src/features/views/recipeBuilder/RecipeBuilder.tsx
@@ -1,11 +1,32 @@
 import { BrowserRouter } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import styles from './landerCard.module.scss';
+import styles from './recipeBuilder.module.scss';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 
 export const RecipeBuilder: React.FC = () => {
   return (
     <div>
+      <div className="recipeBuilderContainer">
+        <div className={styles.brewSectionSelector}>
+          <Container>
+            <Row>
+              <Col className="d-grid gap-1">
+                <Button className={styles.startButton} variant="success">Start</Button>
+              </Col>
+              <Col className="d-grid gap-1">
+                <Button className={styles.pourButton} variant="warning">Pour</Button>
+              </Col>
+              <Col className="d-grid gap-1">
+                <Button className={styles.endButton} variant="danger">End</Button>
+              </Col>
+            </Row>
+          </Container>
+          <hr />
+        </div >
+      </div >
     </div >
   )
 };
